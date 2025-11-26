1d30060741f8f95a67839c1f7f034c5805bd5f59
Jaina <jainamicah@gmail.com>
2025-11-26 20:02:54 +0000

    added navbar.jsx

---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">HoneyHomes</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
