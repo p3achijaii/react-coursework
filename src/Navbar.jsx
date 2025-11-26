8f60ca54ca0975c238c676b7d33de7d21348c8ea
Jaina <jainamicah@gmail.com>
2025-11-26 21:01:29 +0000

    added classnames

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,29 +4,33 @@ function Navbar() {
       <nav>
         <ul>
           <li>
-            <a href="#">HoneyHomes</a>
+            <a href="#" className="logo">
+              HoneyHomes
+            </a>
           </li>
-          <li>
-            <a href="#">Buy</a>
-          </li>
-          <li>
-            <a href="#">Rent</a>
-          </li>
-          <li>
-            <a href="#">Agents</a>
-          </li>
-          <li>
-            <a href="#">About Us</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-          <li>
-            <a href="#">Saved</a>
-          </li>
-          <li>
-            <a href="#">Sign In</a>
-          </li>
+          <div className="nav-links">
+            <li>
+              <a href="#">Buy</a>
+            </li>
+            <li>
+              <a href="#">Rent</a>
+            </li>
+            <li>
+              <a href="#">Agents</a>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">Saved</a>
+            </li>
+            <li>
+              <a href="#">Sign In</a>
+            </li>
+          </div>
         </ul>
       </nav>
     </header>
