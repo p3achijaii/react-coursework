18183df55c462e58ca6e240857cef7b3c6a756a8
Jaina <jainamicah@gmail.com>
2025-11-26 20:12:51 +0000

    added other links into navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,6 +6,24 @@ function Navbar() {
           <li>
             <a href="#">HoneyHomes</a>
           </li>
+          <li>
+            <a href="#">Buy</a>
+          </li>
+          <li>
+            <a href="#">Rent</a>
+          </li>
+          <li>
+            <a href="#">Agents</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">Sign In</a>
+          </li>
         </ul>
       </nav>
     </header>
