706b4f8b4db75d5833f7d1d53590aec63772c5c4
Jaina <jainamicah@gmail.com>
2025-11-26 21:07:50 +0000

    removed classnames and div

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,37 +1,35 @@
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="nav-container">
-        <li>
-          <a href="#" className="nav-logo">
-            HoneyHomes
-          </a>
-        </li>
-      </div>
-      <ul className="nav-links">
-        <li>
-          <a href="#">Buy</a>
-        </li>
-        <li>
-          <a href="#">Rent</a>
-        </li>
-        <li>
-          <a href="#">Agents</a>
-        </li>
-        <li>
-          <a href="#">About Us</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-        <li>
-          <a href="#">Saved</a>
-        </li>
-        <li>
-          <a href="#">Sign In</a>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">HoneyHomes</a>
+          </li>
+          <li>
+            <a href="#">Buy</a>
+          </li>
+          <li>
+            <a href="#">Rent</a>
+          </li>
+          <li>
+            <a href="#">Agents</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">Saved</a>
+          </li>
+          <li>
+            <a href="#">Sign In</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
