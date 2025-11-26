e909a0dd25f38eb7583f51436e6669928cdc6220
Jaina <jainamicah@gmail.com>
2025-11-26 21:04:32 +0000

    added div and classnames

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,39 +1,37 @@
 function Navbar() {
   return (
-    <header className="navbar">
-      <nav>
-        <ul>
-          <li>
-            <a href="#" className="logo">
-              HoneyHomes
-            </a>
-          </li>
-          <div className="nav-links">
-            <li>
-              <a href="#">Buy</a>
-            </li>
-            <li>
-              <a href="#">Rent</a>
-            </li>
-            <li>
-              <a href="#">Agents</a>
-            </li>
-            <li>
-              <a href="#">About Us</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-            <li>
-              <a href="#">Saved</a>
-            </li>
-            <li>
-              <a href="#">Sign In</a>
-            </li>
-          </div>
-        </ul>
-      </nav>
-    </header>
+    <nav className="navbar">
+      <div className="nav-container">
+        <li>
+          <a href="#" className="nav-logo">
+            HoneyHomes
+          </a>
+        </li>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <a href="#">Buy</a>
+        </li>
+        <li>
+          <a href="#">Rent</a>
+        </li>
+        <li>
+          <a href="#">Agents</a>
+        </li>
+        <li>
+          <a href="#">About Us</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+        <li>
+          <a href="#">Saved</a>
+        </li>
+        <li>
+          <a href="#">Sign In</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
