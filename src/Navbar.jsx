b478ef9ad73e2959df33f799e6aa04bc23ef46de
Jaina <jainamicah@gmail.com>
2025-12-16 14:18:18 +0000

    updated the functionality of the navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,36 +1,22 @@
-function Navbar() {
-  return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <a href="#">HoneyHomes</a>
-          </li>
-          <li>
-            <a href="#">Buy</a>
-          </li>
-          <li>
-            <a href="#">Rent</a>
-          </li>
-          <li>
-            <a href="#">Agents</a>
-          </li>
-          <li>
-            <a href="#">About Us</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-          <li>
-            <a href="#">Saved</a>
-          </li>
-          <li>
-            <a href="#">Sign In</a>
-          </li>
-        </ul>
+import { useState } from "react";
+
+function Navbar({ favourites, theme, setTheme }) {
+  const [open, setOpen] = useState(false);
+
+  return(
+    <header className="styles.navbar" role = "navigation" aria-label="Main navigation">
+      <div className="styles.inner">
+        <img src="" alt="HoneyHomes logo"/>
+        <span>HoneyHomes</span>
+      </div>
+
+      <nav className="styles.nav">
+        <a href="#">Buy</a>
+        <a href="#">Rent</a>
+        <a href="#">Sell</a>
       </nav>
     </header>
-  );
+  )
 }
 
 export default Navbar;
