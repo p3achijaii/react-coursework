4681417035f07dd3d6e1be22a88075c93abbfc8b
Jaina <jainamicah@gmail.com>
2025-12-16 18:09:28 +0000

    fix app.jsx: set up routing, pass favourites and theme props to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar.jsx";
 
 function App() {
+  const [favourites, setFavourites] = useState([]);
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
   return (
-    <>
-      <Navbar />
-    </>
+    <BrowserRouter>
+      <Navbar favourites={favourites} theme={theme} setTheme={setTheme} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home favourites={favourites} setFavourites={setFavourites} />
+          }
+        />
+        <Route
+          path="/property/:id"
+          element={
+            <Property favourites={favourites} setFavourites={setFavourites} />
+          }
+        />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
