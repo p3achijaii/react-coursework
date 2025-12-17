844cd0417367c4c8692ffa064a7bbdc9ea0f93e5
Jaina <jainamicah@gmail.com>
2025-12-17 22:47:12 +0000

    fixed code so it only shows the navbar in the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
-import { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import Home from "./components/HomePage.jsx";
-import Footer from "./components/Footer.jsx";
+
+import "./App.css";
 
 function App() {
-  const [favourites, setFavourites] = useState([]);
-  const [theme, setTheme] = useState("light");
-
-  useEffect(() => {
-    document.documentElement.setAttribute("data-theme", theme);
-  }, [theme]);
-
   return (
     <BrowserRouter>
-      <Navbar favourites={favourites} theme={theme} setTheme={setTheme} />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Home favourites={favourites} setFavourites={setFavourites} />
-          }
-        />
-        <Route
-          path="/property/:id"
-          element={
-            <Property favourites={favourites} setFavourites={setFavourites} />
-          }
-        />
-      </Routes>
-      <Footer />
+      <Navbar />
     </BrowserRouter>
   );
 }
