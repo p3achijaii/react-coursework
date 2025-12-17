7be33cd270f5c8a39f8903f85095f42897ce0fb7
Jaina <jainamicah@gmail.com>
2025-12-17 19:18:44 +0000

    added useState for dropdown toggle and imported useLocation for route-aware navigation

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,4 +1,14 @@
-function Navbar() {}
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false)
+  const location = useLocation()
+
+  const links = [
+    {href: '/', label: 'Home'}
+  ]
+}
 
 export default Navbar;
-
