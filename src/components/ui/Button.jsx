81da13eaa3fe1d2cebf88673a64548132c8cce8e
Jaina <jainamicah@gmail.com>
2025-12-18 15:51:05 +0000

    named function ButtonComponent instead of an anonymous arrow function

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -2,30 +2,30 @@ import React, { forwardRef } from "react";
 import styles from "./Button.module.css";
 import { cn } from "../utils";
 
-const Button = forwardRef(
-  (
-    {
-      className,
-      variant = "primary",
-      size = "md",
-      isLoading = false,
-      children,
-      ...props
-    },
-    ref
-  ) => {
-    return (
-      <button
-        ref={ref}
-        className={cn(styles.button, styles[variant], styles[size], className)}
-        disabled={isLoading}
-        {...props}
-      >
-        {isLoading && <div className={styles.spinner} />}
-      </button>
-    );
-  }
-);
+function ButtonComponent(
+  {
+    className,
+    variant = "primary",
+    size = "md",
+    isLoading = false,
+    children,
+    ...props
+  },
+  ref
+) {
+  return (
+    <button
+      ref={ref}
+      className={cn(styles.button, styles[variant], styles[size], className)}
+      disabled={isLoading}
+      {...props}
+    >
+      {isLoading && <div className={styles.spinner} />}
+    </button>
+  );
+}
+
+const Button = forwardRef(ButtonComponent);
 
 Button.displayName = "Button";
 
