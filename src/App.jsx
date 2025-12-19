bd1f1b29cb11b12ed2259cc87c35ea15529f9f90
Jaina <jainamicah@gmail.com>
2025-12-19 22:50:42 +0000

    imported footer.jsx to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import HomePage from "./pages/HomePage/HomePage.jsx";
+import Footer from "./pages/Footer/Footer.jsx";
 
 import "./App.css";
 
@@ -13,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
+
+      <Footer />
     </BrowserRouter>
   );
 }
