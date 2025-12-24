3ac8edae748ee606eb2d7eaeafd05a459513ff6a
Jaina <jainamicah@gmail.com>
2025-12-24 17:20:19 +0000

    add the imports for the market property page

---
+++ src/pages/MarketProperty/MarketProperty.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "../../components/ui/Button";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
+import styles from "./MarketProperty.module.css";
+
+function MarketProperty() {}
+
+export default MarketProperty;
