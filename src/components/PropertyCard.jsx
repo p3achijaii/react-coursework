298eef60a0f096876b42d2c14c83725475ee583e
Jaina <jainamicah@gmail.com>
2025-12-29 13:53:18 +0000

    add drag-and-drop and favourite handlers to propertycard

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -6,6 +6,24 @@ import { formatPrice } from "../components/utils";
 import { cn } from "../components/utils";
 import styles from "../components/PropertyCard.module.css";
 
-function PropertyCard() {}
+function PropertyCard({
+  property,
+  draggable = true,
+  isFavourite = false,
+  onToggleFavourirte,
+}) {
+  const handleDragStart = (e) => {
+    if (!draggable) return;
+
+    e.dataTransfer.setData("application/json", JSON.stringify(property));
+    e.dataTransfer.effectAllowed = "move";
+  };
+
+  const handleFavouriteClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    onToggleFavourirte?.(property);
+  };
+}
 
 export default PropertyCard;
