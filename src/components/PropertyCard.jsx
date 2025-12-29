1ed3a4454a9192f5f86d7fae9fba24157166f416
Jaina <jainamicah@gmail.com>
2025-12-29 13:48:20 +0000

    set up propertycard with styles and icon imports

---
+++ src/components/PropertyCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { MapPin, Bed, Heart, FileText } from "lucide-react";
+
+import { formatPrice } from "../components/utils";
+import { cn } from "../components/utils";
+import styles from "../components/PropertyCard.module.css";
+
+function PropertyCard() {}
+
+export default PropertyCard;
