e6252efb237b9cf97b111b1af44265ee28967c5a
Jaina <jainamicah@gmail.com>
2025-12-31 19:37:09 +0000

    set up findproperty page with imports and initial setup

---
+++ src/pages/FindProperty/FindProperty.jsx
@@ -0,0 +1,10 @@
+import React, { useMemo, useState } from "react";
+import { SearchFilters } from "../../components/SearchFilters";
+import { PropertyCard } from "../../components/PropertyCard";
+
+import { properties } from "../../../public/properties.json";
+import styles from "./FindProperty.module.css";
+
+function FindProperty() {}
+
+export default FindProperty;
