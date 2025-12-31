8eceb066926b9931e4a1a2cfec7b4f2435b86657
Jaina <jainamicah@gmail.com>
2025-12-31 20:12:28 +0000

    add filters setup and filtered property list

--- src/pages/FindProperty/FindProperty.jsx
+++ src/pages/FindProperty/FindProperty.jsx
@@ -1,10 +1,25 @@
 import React, { useMemo, useState } from "react";
 import { SearchFilters } from "../../components/SearchFilters";
 import { PropertyCard } from "../../components/PropertyCard";
-
+import { FavoritesSidebar } from "../../components/FavoritesSidebar";
 import { properties } from "../../../public/properties.json";
 import styles from "./FindProperty.module.css";
 
-function FindProperty() {}
+function FindProperty() {
+  const [filter, setFilters] = useState({
+    type: "",
+    minPrice: "",
+    maxPrice: "",
+    beds: "",
+    postcode: "",
+    dateAdded: "",
+  });
+
+  const filteredProperties = useMemo(() => {
+    return properties.filter((property) => {
+      if (filters.type && property.type !== filters.type) return false;
+    });
+  });
+}
 
 export default FindProperty;
