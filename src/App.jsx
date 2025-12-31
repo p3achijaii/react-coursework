530c4e219cf3c1627447d1ff8e844568044f3718
Jaina <jainamicah@gmail.com>
2025-12-31 14:05:56 +0000

    remove findproperty import from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import HomePage from "./pages/HomePage/HomePage.jsx";
-import FindProperty from "./pages/FindProperty/FindProperty.jsx";
 import MarketProperty from "./pages/MarketProperty/MarketProperty.jsx";
 import WhyHoneyHomes from "./pages/WhyHoneyhomes/WhyHoneyhomes.jsx";
 import Contact from "./pages/Contact/Contact.jsx";
@@ -19,7 +18,6 @@ function App() {
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/find-property" element={<FindProperty />} />
         <Route path="/market-property" element={<MarketProperty />} />
         <Route path="/why-honeyhomes" element={<WhyHoneyHomes />} />
         <Route path="/contact" element={<Contact />} />
