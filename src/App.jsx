2827425b36750daf105f4372970347f030f2825e
Jaina <jainamicah@gmail.com>
2026-01-01 23:31:13 +0000

    remove unused styles from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,20 @@ import Footer from "./pages/Footer/Footer.jsx";
 import FavoritesProvider from "./contexts/FavoritesContext.jsx";
 import ScrollToTop from "./components/ui/ScrollToTop.jsx";
 
-import "./App.css";
-
 function App() {
   return (
     <FavoritesProvider>
       <BrowserRouter>
-        <div className={styles.app}>
-          <ScrollToTop />
-          <Navbar />
-          <main className={styles.main}>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/find-property" element={<FindProperty />} />
-              <Route path="/market-property" element={<MarketProperty />} />
-              <Route path="/why-honeyhomes" element={<WhyHoneyHomes />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+        <ScrollToTop />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/find-property" element={<FindProperty />} />
+          <Route path="/market-property" element={<MarketProperty />} />
+          <Route path="/why-honeyhomes" element={<WhyHoneyHomes />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </FavoritesProvider>
   );
