5245ff930b7d49fa54fc882da24254c5d5c9ca17
Jaina <jainamicah@gmail.com>
2026-01-04 21:12:04 +0000

    install and configure jest testing dependencies

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+export default {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+};
